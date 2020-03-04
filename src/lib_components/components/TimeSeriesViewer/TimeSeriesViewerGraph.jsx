a7355ec027aab0ddb1edc7c817fcdf62ae9519b9
Frencil <frencils@gmail.com>
2020-03-04 13:55:55 -0700

    functional graph legend with highlight

--- src/lib_components/components/TimeSeriesViewer/TimeSeriesViewerGraph.jsx
+++ src/lib_components/components/TimeSeriesViewer/TimeSeriesViewerGraph.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useRef, useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import Dygraph from 'dygraphs';
 import 'dygraphs/dist/dygraph.min.css';
 
@@ -6,6 +8,19 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import TimeSeriesViewerContext, { TIME_SERIES_VIEWER_STATUS } from './TimeSeriesViewerContext';
 import Theme from '../Theme/Theme';
+
+const COLORS = [
+  '#4e79a7',
+  '#f28e2c',
+  '#e15759',
+  '#76b7b2',
+  '#59a14f',
+  '#edc949',
+  '#af7aa1',
+  '#ff9da7',
+  '#9c755f',
+  '#bab0ab',
+];
 
 const BASE_GRAPH_OPTIONS = {
   includeZero: true,
@@ -16,26 +31,57 @@ const BASE_GRAPH_OPTIONS = {
   connectSeparatedPoints: false,
   rangeSelectorPlotFillColor: Theme.palette.primary.light,
   animatedZooms: true,
-  /*
-    highlightSeriesOpts: {
-    strokeWidth: 2,
-    },
-  */
-  /*
-    logscale: this.props.timeSeries.graph.isLogScaled,
-    labelsDiv: this.legend,
-    legend: "always",
-    legendFormatter: this.renderLegend,
-  */
+  colors: COLORS,
+  highlightSeriesOpts: {
+    strokeWidth: 1.5,
+  },
 };
 
+const boxShadow = '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)';
 const useStyles = makeStyles(theme => ({
   graphContainer: {
     padding: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'flex-start',
   },
   graphDiv: {
-    width: '100%',
     height: '320px',
+    flexGrow: 1,
+  },
+  legendDiv: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(1),
+  },
+  legendSeries: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexWrap: 'nowrap',
+    padding: theme.spacing(1),
+    borderRadius: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    backgroundColor: theme.palette.grey[50],
+    boxShadow,
+  },
+  legendSeriesX: {
+    whiteSpace: 'nowrap',
+    padding: theme.spacing(1),
+    borderRadius: theme.spacing(1),
+    backgroundColor: theme.palette.grey[100],
+    textAlign: 'center',
+    fontWeight: 600,
+    boxShadow,
+  },
+  legendSeriesColor: {
+    width: theme.spacing(3),
+    height: '4px',
+    borderRadius: '2px',
+  },
+  legendSeriesLabel: {
+    whiteSpace: 'nowrap',
+    lineHeight: '1rem',
+    fontSize: '0.8rem',
+    marginLeft: theme.spacing(1),
   },
 }));
 
@@ -44,6 +90,7 @@ export default function TimeSeriesViewerGraph() {
   const [state] = TimeSeriesViewerContext.useTimeSeriesViewerState();
   const dygraphRef = useRef(null);
   const dygraphDomRef = useRef(null);
+  const legendRef = useRef(null);
   const axisCountRef = useRef(1);
   const axisCountChangedRef = useRef(false);
   const {
@@ -67,12 +114,18 @@ export default function TimeSeriesViewerGraph() {
 
   // Build the first column (dateTime) and keep track of where the month breaks are
   const buildTimeData = () => {
+    const downloadPkg = 'basic';
+    const source = state.product.sites[sites[0].siteCode].positions[sites[0].positions[0]];
     continuousDateRange.forEach((month, idx) => {
       monthOffsets[idx] = data.length;
-      state.product.sites[sites[0].siteCode].positions[sites[0].positions[0]]
-        .data[month].basic[timeStep].series.startDateTime.forEach((d) => {
-          data.push([d]);
-        });
+      if (
+        !source.data[month]
+          || !source.data[month][downloadPkg]
+          || !source.data[month][downloadPkg][timeStep]
+      ) { return; }
+      source.data[month][downloadPkg][timeStep].series.startDateTime.forEach((d) => {
+        data.push([d]);
+      });
     });
   };
 
@@ -83,7 +136,7 @@ export default function TimeSeriesViewerGraph() {
       positions.forEach((position) => {
         continuousDateRange.forEach((month, monthIdx) => {
           variables.forEach((variable) => {
-            const label = `${siteCode} ${position} - ${variable}`;
+            const label = `${siteCode} - ${position} - ${variable}`;
             if (!labels.includes(label)) {
               labels.push(label);
               series.push({
@@ -96,10 +149,15 @@ export default function TimeSeriesViewerGraph() {
             }
             const columnIdx = labels.indexOf(label);
             const { downloadPkg } = state.variables[variable];
-            state.product.sites[siteCode].positions[position]
-              .data[month][downloadPkg][timeStep].series[variable].forEach((d, datumIdx) => {
-                data[datumIdx + monthOffsets[monthIdx]][columnIdx] = d;
-              });
+            const source = state.product.sites[siteCode].positions[position];
+            if (
+              !source.data[month]
+                || !source.data[month][downloadPkg]
+                || !source.data[month][downloadPkg][timeStep]
+            ) { return; }
+            source.data[month][downloadPkg][timeStep].series[variable].forEach((d, datumIdx) => {
+              data[datumIdx + monthOffsets[monthIdx]][columnIdx] = d;
+            });
           });
         });
       });
@@ -110,7 +168,7 @@ export default function TimeSeriesViewerGraph() {
   const buildAxesOption = (axes = []) => {
     const axesOption = {
       x: {
-        valueFormatter: Dygraph.dateString_, // eslint-disable-line no-underscore-dangle
+        valueFormatter: Dygraph.dateString_,
         ticker: Dygraph.dateTicker,
         axisLabelFormatter: Dygraph.dateAxisLabelFormatter,
       },
@@ -126,15 +184,50 @@ export default function TimeSeriesViewerGraph() {
   // Build the series option
   const buildSeriesOption = (axes = []) => {
     const seriesOption = {};
-    series.forEach((serie) => {
-      const axis = axes.find(a => a.units === serie.units);
+    series.forEach((s) => {
+      const axis = axes.find(a => a.units === s.units);
       if (!axis) { return; }
-      seriesOption[serie.label] = {
+      seriesOption[s.label] = {
         axis: axis.axis,
       };
     });
     return seriesOption;
   };
+
+  const legendFormatter = graphData => ReactDOMServer.renderToString(
+    <React.Fragment>
+      {graphData.series.map((s, idx) => {
+        const { units } = series[idx];
+        let yUnits = units;
+        if (typeof s.y !== 'undefined') {
+          let yStr = s.y.toString();
+          if (yStr.indexOf('.') !== -1 && yStr.length - yStr.indexOf('.') > 3) {
+            yStr = s.y.toFixed(2).toString();
+          }
+          const yUnitsStr = `${yStr} ${units}`;
+          yUnits = s.isHighlighted ? <b>{yUnitsStr}</b> : yUnitsStr;
+        }
+        const seriesStyle = s.isHighlighted ? { backgroundColor: Theme.palette.grey[100] } : {};
+        const colorStyle = { backgroundColor: s.color };
+        if (s.isHighlighted) { colorStyle.height = '6px'; }
+        return (
+          <div key={s.label} className={classes.legendSeries} style={seriesStyle}>
+            <div className={classes.legendSeriesColor} style={colorStyle} />
+            <div className={classes.legendSeriesLabel}>
+              {s.label}
+              <br />
+              {yUnits}
+            </div>
+          </div>
+        );
+      })}
+      {graphData.x ? (
+        <div className={classes.legendSeriesX}>
+          {graphData.xHTML}
+        </div>
+      ) : null}
+    </React.Fragment>,
+  );
 
   if (state.status === TIME_SERIES_VIEWER_STATUS.READY) {
     buildTimeData();
@@ -157,6 +250,9 @@ export default function TimeSeriesViewerGraph() {
       series: buildSeriesOption(axes),
       rollPeriod,
       logscale,
+      labelsDiv: legendRef.current,
+      legend: 'always',
+      legendFormatter,
     };
     // Apply axis labels to graphOptions
     axes.forEach((axis) => {
@@ -166,8 +262,13 @@ export default function TimeSeriesViewerGraph() {
 
   useEffect(() => {
     if (state.status !== TIME_SERIES_VIEWER_STATUS.READY) { return; }
+    const redrawWidth = () => {
+      dygraphRef.current.graphDiv.style.width = null;
+      dygraphRef.current.resizeHandler_();
+    };
     if (dygraphRef.current === null) {
       dygraphRef.current = new Dygraph(dygraphDomRef.current, data, graphOptions);
+      redrawWidth();
     } else {
       dygraphRef.current.updateOptions({ file: data, ...graphOptions });
       // Dygraphs has a bug where the canvas isn't cleared properly when dynamically changing
@@ -177,22 +278,17 @@ export default function TimeSeriesViewerGraph() {
         dygraphRef.current.updateOptions({ showRangeSelector: false });
         dygraphRef.current.updateOptions({ showRangeSelector: true });
       }
+      redrawWidth();
     }
   }, [selectionDigest, state.status, data, graphOptions, dygraphRef, axisCountChangedRef]);
 
   /**
      RENDER
   */
-  /*
-  const renderPre = () => (state.status === TIME_SERIES_VIEWER_STATUS.READY ? (
-    <pre style={{ ...preStyle, height: '45vh' }}>
-      {JSON.stringify({ graphOptions, data }, null, 2)}
-    </pre>
-  ) : null);
-  */
   return (
     <div className={classes.graphContainer}>
-      <div ref={dygraphDomRef} className={classes.graphDiv} />
+      <div ref={dygraphDomRef} className={classes.graphDiv} style={{ width: '50% !important' }} />
+      <div ref={legendRef} className={classes.legendDiv} />
     </div>
   );
 }
