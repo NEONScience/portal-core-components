46c5159679a531e83e57900a75f8d1c00ab4edcf
Frencil <frencils@gmail.com>
2020-04-07 12:01:39 -0600

    add Open in New Window link

--- src/lib_components/components/StoryMap/StoryMap.jsx
+++ src/lib_components/components/StoryMap/StoryMap.jsx
@@ -1,13 +1,33 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+
+import Theme from '../Theme/Theme';
 import FullWidthVisualization from '../FullWidthVisualization/FullWidthVisualization';
 
 const MIN_IFRAME_WIDTH = 240;
 
+const useStyles = makeStyles(theme => ({
+  openInNewLink: {
+    display: 'block',
+    width: '100%',
+    textAlign: 'right',
+    marginTop: theme.spacing(0.5),
+    fontSize: '0.8rem',
+  },
+  openInNewIcon: {
+    fontSize: '0.95rem',
+    margin: theme.spacing(0, 0.5, -0.25, 0),
+  },
+}));
+
 const StoryMap = (props) => {
   const { url, title = 'NEON Story Map' } = props;
 
+  const classes = useStyles(Theme);
   const iframeRef = useRef(null);
 
   return (
@@ -26,6 +46,10 @@ const StoryMap = (props) => {
         marginWidth="0"
         style={{ minWidth: `${MIN_IFRAME_WIDTH}px`, minHeight: `${MIN_IFRAME_WIDTH}px` }}
       />
+      <Link href={url} target="_blank" className={classes.openInNewLink}>
+        <OpenInNewIcon className={classes.openInNewIcon} />
+        Open in New Window
+      </Link>
     </FullWidthVisualization>
   );
 };
