57a40dcc7db506024de1bebed41727a9b760fb1e
Frencil <frencils@gmail.com>
2020-07-22 10:32:25 -0600

    fix NeonJsonLd lib export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -22,6 +22,7 @@ export { default as NeonFooter } from './components/NeonFooter';
 export { default as NeonHeader } from './components/NeonHeader';
 export { default as NeonMenu } from './components/NeonMenu';
 export { default as NeonUtilityBar } from './components/NeonUtilityBar';
+export { default as NeonJsonLd } from './components/NeonJsonLd';
 export { default as DialogBase } from './components/DialogBase';
 export { default as PopupBase } from './components/PopupBase';
 export { default as PopupLoading } from './components/PopupLoading';
