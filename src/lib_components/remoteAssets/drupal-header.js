5044b1b8d73ad5be3182b00373988eeb074d68d9
Frencil <frencils@gmail.com>
2020-10-21 13:56:39 -0600

    latest drupal header/footer assets

--- src/lib_components/remoteAssets/drupal-header.js
+++ src/lib_components/remoteAssets/drupal-header.js
@@ -1,96 +1,22 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({2:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({3:[function(require,module,exports){
 "use strict";
 
 (function ($) {
   $(document).ready(function () {
+    try {
+      onReady();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  function onReady() {
     // All screens
     var $header = $(".header");
     var mainNavHeight = $header.outerHeight() || 0;
     var mainNavOffset = $header.parent().offset();
-
     $header.parent().css("padding-top", mainNavHeight + "px");
     $header.css("top", mainNavOffset.top + "px");
-
-    $.fn.cssPseudo = function (pseudoClass, cssObject, customSelectorString) {
-      var sheet = ($.cssPseudo || {}).sheet || document.styleSheets[0],
-          //  use $.cssPseudo.createSeperateStylesheet() to create a seperate style sheet
-      $self = this,
-          customSelectorStringTpl = customSelectorString ? !~customSelectorString.indexOf("%s") ? "%s" + customSelectorString : customSelectorString : "%s",
-          result = false;
-
-      // clear statements, the user must set it to a string or to false.
-      if (pseudoClass === undefined) throw new Error("pseudoClass must not be undefined");
-      // normalize
-      var psCl = pseudoClass === false || pseudoClass === null || pseudoClass === "" ? null : pseudoClass && pseudoClass.trim() ? pseudoClass.trim() : null;
-      // clear statements, the user must set it to an object or to false
-      if (psCl !== null && cssObject === undefined) throw new Error("cssObject must not be undefined, when pseudoClass is defined");
-      // normalize
-      cssObject = cssObject !== false && cssObject !== null && cssObject !== undefined && cssObject !== "" ? cssObject : null;
-
-      // iterate all jQuery-selected elements
-      this.each(function () {
-        var d = $(this).data("css-pseudos");
-
-        if (!d) {
-          $(this).data("css-pseudos", d = []);
-        }
-        if (cssObject && typeof cssObject !== "string") {
-          // is object, add
-          var uid = $(this).attr("data-css-pseudo-uid");
-          if (!uid) $(this).attr("data-css-pseudo-uid", uid = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now());
-          var parsedCss = $("<css-pseudo-temp>").css(cssObject).attr("style"),
-              cssStr;
-          var idx = sheet.insertRule(cssStr = customSelectorStringTpl.replace("%s", '[data-css-pseudo-uid="' + uid + '"]') + (psCl ? ":" + psCl : "") + " { " + parsedCss + " }");
-          var rule = sheet.rules[idx];
-          d.push({
-            pseudoClass: psCl,
-            customSelectorString: customSelectorString,
-            rule: rule
-          });
-          //console.log('rules length: ', sheet.rules.length, '-- cssStr:', cssStr);
-        } else if (typeof cssObject == "string") {
-          // is string
-          // only last value is used.
-          result = undefined;
-          $(d).each(function (i) {
-            if (this.pseudoClass == psCl && this.customSelectorString == customSelectorString) result = $("<css-pseudo-temp>").attr("style", this.rule.cssText.match(/{(.+)}/).pop()).css(cssObject); // only last value is used.
-          });
-        } else {
-          // remove -> cssObject === null
-          var ret = $(d).filter(function (i) {
-            if ((psCl === null || this.pseudoClass == psCl) && (!customSelectorString || this.customSelectorString == customSelectorString)) {
-              var idx = $.inArray(this.rule, sheet.rules);
-              if (idx !== -1) sheet.deleteRule(idx);
-              return false; // remove rule from knonwn ones
-            }
-            return true; // keep rule
-          });
-          if (ret.length)
-            // some rules are left over
-            $(this).data("css-pseudos", ret);else {
-            $(this).removeData("css-pseudos");
-            $(this).removeAttr("data-css-pseudo-uid");
-          }
-        }
-      });
-      return result !== false ? result : this; // we went into getting a value, so no elements will be returned but a string
-    };
-    /*
-     * configure the cssPseudo plugin
-     */
-    $.cssPseudo = {
-      sheet: document.styleSheets[0], // internally used
-      /**
-       * automatically create a style element, or use one provided by the `optionalCreatedStylesheetElement` param
-       */
-      createSeperateStylesheet: function $_cssPseudo_createSeperateStylesheet(optionalCreatedStylesheetElement) {
-        this.sheet = (optionalCreatedStylesheetElement || $('<style ref="cssPseudo">').appendTo("head").get(0)).sheet;
-      }
-    };
-    // The below polyfill will bring support to IE5-IE8.
-    // https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/deleteRule
-    if (!CSSStyleSheet.prototype.deleteRule) CSSStyleSheet.prototype.deleteRule = CSSStyleSheet.prototype.removeRule;
-    if (!CSSStyleSheet.prototype.insertRule) CSSStyleSheet.prototype.insertRule = CSSStyleSheet.prototype.addRule;
 
     // Throlle Function
     function throttle(fn, wait) {
@@ -142,43 +68,15 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       // Desktop
       if ($(window).width() > 1200) {
         var megaMenuOffset = mainNavHeight + mainNavOffset.top + "px";
-        // let megaMenuOffsetLessHeader = mainNavOffset.top + "px";
-
         $(".subNavWrapper").each(function () {
-          // Sub Nav Wrapper add a height that's calc 100vh - (mainNavHeight + mainNavOffset.top)
-          // Sub Nav Wrapper set the top value to mainNavHeight + mainNavOffset.top
 
           $(this).css({
             height: "calc(100vh - " + megaMenuOffset + ")",
-            //'height': 'calc(100vh)',
             top: megaMenuOffset
           });
 
-          // $(this).cssPseudo('after', {
-          //   content: '',
-          //   // height: '100vh'
-          //   height:  'calc(100vh + ' + megaMenuOffset + ')'
-          // });
-          // $(this).cssPseudo('before', {
-          //   content: '',
-          //   //height: '100vh'
-          //   height:  'calc(100vh + ' + megaMenuOffset + ')'
-          // });
-
           $(this).find(".innerSubNavWrapper").css({
             height: "calc(100vh - " + megaMenuOffset + ")"
-          });
-
-          // $(this).find('.innerSubNavWrapper').css('height', 'calc(100vh + ' - megaMenuOffset + ')')
-          $(this).find(".innerSubNavWrapper").cssPseudo("before", {
-            content: "",
-            //height: '100vh'
-            height: "calc(100vh + " + megaMenuOffset + ")"
-          });
-          $(this).find(".innerSubNavWrapper").cssPseudo("after", {
-            content: "",
-            //height: '100vh'
-            height: "calc(100vh + " + megaMenuOffset + ")"
           });
         });
 
@@ -209,17 +107,6 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
 
     $("nav#block-neon-main-menu > ul.menu--main > li.menu__item--expanded").each(function (e) {
       var topLevelNavLabel = $(this).children("a").text();
-      // $(this).children("a.menu__link").after(
-      //   "<button class='arrow visually-hidden focusable' tabindex='0' aria-label='Toggle Mega Menu'><svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 1L5 5 1 1' stroke='#fff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg></button>"
-      // );
-
-      // $(this)
-      //   .children(".subNavWrapper")
-      //   .append(
-      //     "<div class='subNavLabelWrapper'><button class='subNavClose isDesktop' aria-label='Close Mega Menu'><svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M.808 17.778L17.778.808l1.414 1.414-16.97 16.97-1.414-1.414z' fill='#fff'/><path d='M2.222.808l16.97 16.97-1.414 1.414L.808 2.222 2.222.808z' fill='#fff'/></svg></button><h3 class='isDesktop'>" +
-      //       topLevelNavLabel +
-      //       "</h3> <button class='mobileBack isMobile' aria-label='Back'><svg width='14' height='10' viewBox='0 0 14 10' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M5 9L1 5l4-4M1 5h12' stroke='#0073CF' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg></button></div>"
-      //   );
     });
 
     $(".subNavWrapper").mouseover(function (e) {
@@ -294,15 +181,9 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
         $("ul[data-depth='0'] > li.menu__item--expanded").removeClass("over");
         $(thisParent).find("ul[data-depth='0'] > li.expandable").removeClass("active");
         $("nav#block-neon-main-menu ul[data-depth='0'] > li.menu__item--expanded .subNavWrapper").each(function (e) {
-          //$(this).find('.subNavWrapper:not(:eq(0))').remove();
           $(this).removeClass("depthZero depthOne depthTwo depthThree depthFour").addClass("subNavWrapper");
         });
-      } else {
-        // $(".subNavWrapper").each(function(e) {
-        //   $(this).removeClass("depthZero depthOne depthTwo depthThree depthFour").addClass("subNavWrapper");
-        // });
-        //$("ul[data-depth='0'] > li.menu__item--expanded").removeClass("over");
-      }
+      } else {}
     });
 
     // Press esc key to close the mega menu
@@ -346,23 +227,23 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       var $MegaMenu3rdCol = $(".subNavWrapper").hasClass("depthOne").toString();
       var $MegaMenu4thCol = $(".subNavWrapper").hasClass("depthThree").toString();
       if ($active === "true") {
-        console.log('active');
+        console.log("active");
         $(this).parent().closest(".subNavWrapper").removeClass("depthOne");
         $(this).parent().closest(".subNavWrapper").removeClass("depthThree");
-        $(this).children().find('li.active').removeClass("active");
+        $(this).children().find("li.active").removeClass("active");
 
         // $(this).parent().parent().children().find('li.active').removeClass("active");
         $(this).addClass("active").siblings("li").removeClass("active");
       } else {
-        $(this).siblings("li").find('li.active').removeClass("active");
+        $(this).siblings("li").find("li.active").removeClass("active");
 
         if ($MegaMenu3rdCol === "true" && $MegaMenu4thCol === "true") {
-          console.log('not active 1');
+          console.log("not active 1");
           $(this).parent().closest(".subNavWrapper").toggleClass("depthThree");
         } else if ($MegaMenu3rdCol === "true") {
-          console.log('not active 2');
+          console.log("not active 2");
         } else {
-          console.log('not active 3');
+          console.log("not active 3");
           $(this).parent().closest(".subNavWrapper").toggleClass("depthOne");
         }
       }
@@ -370,7 +251,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
 
     $('nav#block-neon-main-menu ul.menu--main[data-depth="2"] > li.expandable').click(function (e) {
       e.stopPropagation();
-      console.log('level two');
+      console.log("level two");
       $(this).parent().closest(".subNavWrapper").toggleClass("depthTwo");
       $(this).parent().closest(".subNavWrapper").toggleClass("depthThree");
     });
@@ -388,7 +269,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
 
     $('nav#block-neon-main-menu ul.menu--main[data-depth="4"] > li.expandable').click(function (e) {
       e.stopPropagation();
-      console.log('level four');
+      console.log("level four");
       //
       $(this).parent().closest(".subNavWrapper").toggleClass("depthFour");
     });
@@ -398,7 +279,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       e.stopPropagation();
       //$(this).toggleClass("active").parents("ul").children("li").removeClass("active");
       $(this).toggleClass("active").siblings("li").removeClass("active");
-      console.log('remove active - Level One');
+      console.log("remove active - Level One");
     });
 
     // Add a class to the sub items that expand more sub lists - FOR the third level
@@ -406,14 +287,14 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       e.stopPropagation();
 
       if ($(this).hasClass("active").toString() === "true") {
-        console.log('true');
+        console.log("true");
         $(this).removeClass("active");
       } else {
-        console.log('false');
+        console.log("false");
         $("ul[data-depth='3']").children("li").removeClass("active");
         $(this).addClass("active").siblings("li").removeClass("active");
       }
-      console.log('remove active - Level Three');
+      console.log("remove active - Level Three");
     });
 
     // Hide/show Desktop Search
@@ -434,7 +315,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
     });
 
     $("#search-block-form label").removeClass("visually-hidden");
-  });
+  }
 })(jQuery);
 
-},{}]},{},[2]);
+},{}]},{},[3]);
