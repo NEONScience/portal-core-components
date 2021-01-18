19ad3cd8c420ca29aa3d6ef71d6c553e95fc630e
Frencil <frencils@gmail.com>
2021-01-18 13:46:24 -0700

    StoryMap and External Host snapshot tests

--- src/lib_components/components/SiteChip/__tests__/SiteChip.jsx
+++ src/lib_components/components/SiteChip/__tests__/SiteChip.jsx
@@ -4,9 +4,24 @@ import renderer from 'react-test-renderer';
 import SiteChip from '../SiteChip';
 
 describe('SiteChip', () => {
-  test('Renders correctly', () => {
+  test('Renders correctly with only a label', () => {
     const tree = renderer
       .create(<SiteChip label="foo" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('Preserves MUI chip props', () => {
+    const chip = (
+      <SiteChip
+        label="foo"
+        size="large"
+        color="primary"
+        variant="default"
+        data-selenium="chip"
+      />
+    );
+    const tree = renderer
+      .create(chip)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
