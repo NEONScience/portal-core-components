035e6446bdd043b20b5ad379ddfb37a614171cb8
jsampson <jsampson@battelleecology.org>
2024-08-14 14:07:54 -0600

    ignore .next in docker, update asset scripts

--- scripts/sync-assets.js
+++ scripts/sync-assets.js
@@ -10,8 +10,8 @@ process.on('unhandledRejection', err => {
 });
 
 import fs from 'fs';
+import readline from 'readline';
 import path, { dirname } from 'path';
-import jsdom from 'jsdom';
 import prettier from 'prettier';
 import { Command } from 'commander';
 import { fileURLToPath } from 'url';
@@ -26,8 +26,12 @@ program.usage('Usage: node sync-assets.js [options]')
     'public folder path relative to current working directory',
   )
   .option(
+    '--cwd-relative-app-dir-path <value>',
+    'app folder path relative to current working directory',
+  )
+  .option(
     '--prettify-index',
-    'option to prettify css file',
+    'option to prettify index file',
   );
 program.parse(process.argv);
 const config = program.opts();
@@ -39,28 +43,37 @@ if ((config.cwdRelativePublicDirPath === null)
   console.error('Invalid command arguments. --cwd-relative-public-dir-path must be specified');
   process.exit(9);
 }
+if ((config.cwdRelativeAppDirPath === null)
+  || (config.cwdRelativeAppDirPath === undefined)
+  || (config.cwdRelativeAppDirPath.length <= 0)
+) {
+  console.error('Invalid command arguments. --cwd-relative-app-dir-path must be specified');
+  process.exit(9);
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const CORE_COMPONENTS_PUBLIC_CSS_ASSETS_PATH = path.join(__dirname, '../public/assets/css');
 
 const PUBLIC_OUTPUT_PATH = path.join(process.cwd(), config.cwdRelativePublicDirPath);
+const APP_OUTPUT_PATH = path.join(process.cwd(), config.cwdRelativeAppDirPath);
 const PUBLIC_ASSETS_CSS_OUTPUT_PATH = path.join(PUBLIC_OUTPUT_PATH, 'assets/css');
+
+const APP_FILE_LINE_REPLACE = 'const DRUPAL_THEME_CSS_ASSET_HASH =';
 
 console.log(`Current working directory:\n\t${process.cwd()}`);
 console.log(`Assets to sync from directory:\n\t${CORE_COMPONENTS_PUBLIC_CSS_ASSETS_PATH}`);
 console.log(`Public directory path:\n\t${PUBLIC_OUTPUT_PATH}\n`);
+console.log(`App directory path:\n\t${APP_OUTPUT_PATH}\n`);
 
 console.log('Synching assets...\n');
 
 const SYNC_CSS_ASSET_PREFIX = {
   DRUPAL_THEME: 'drupal-theme',
-  DRUPAL_FONTS: 'drupal-fonts',
 };
 const SYNC_CSS_ASSET_INDEX = {
   DRUPAL_THEME: {
     prefix: 'drupal-theme',
-    dataMeta: 'drupal-theme',
   },
 };
 
@@ -89,26 +102,37 @@ const syncCssAssets = () => {
     Object.keys(SYNC_CSS_ASSET_INDEX).forEach(async (assetKey) => {
       const assetIndex = SYNC_CSS_ASSET_INDEX[assetKey];
       if (assetFileName.startsWith(assetIndex.prefix)) {
-        const indexFilePath = path.join(PUBLIC_OUTPUT_PATH, 'index.html');
-        if (fs.existsSync(indexFilePath)) {
-          const indexFileData = fs.readFileSync(indexFilePath);
-          const dom = new jsdom.JSDOM(indexFileData);
-          const assetNode = dom.window.document.head
-            .querySelector(`link[data-meta="${assetIndex.dataMeta}"]`);
-          if (assetNode) {
-            assetNode.setAttribute('href', `%PUBLIC_URL%/assets/css/${assetFileName}`)
+        const appFilePath = path.join(APP_OUTPUT_PATH, 'layout.tsx');
+        if (fs.existsSync(appFilePath)) {
+          const fileNameHash = assetFileName.split('.')[1];
+          const rl = readline.createInterface({
+            input: fs.createReadStream(appFilePath),
+            // Considers \r and \n as a single newline.
+            // https://nodejs.org/api/readline.html#readlinepromisescreateinterfaceoptions
+            crlfDelay: Infinity,
+          });
+          const lines = [];
+          for await (const line of rl) {
+            if (line.startsWith(APP_FILE_LINE_REPLACE)) {
+              const updatedHashLine = `${APP_FILE_LINE_REPLACE} '${fileNameHash}';`;
+              lines.push(updatedHashLine);
+            } else {
+              lines.push(line);
+            }
           }
-          let indexFileOutput = dom.serialize();
+          rl.close();
+          let appFileOutput = lines.join('\n');
           if (config.prettifyIndex) {
-            indexFileOutput = await prettier.format(
-              indexFileOutput,
+            appFileOutput = await prettier.format(
+              appFileOutput,
               {
-                parser: 'html',
+                parser: 'babel-ts',
                 printWidth: 100,
+                singleQuote: true,
               },
             );
           }
-          fs.writeFileSync(indexFilePath, indexFileOutput, { encoding: 'utf8' });
+          fs.writeFileSync(appFilePath, appFileOutput, { encoding: 'utf8' });
         }
       }
     });
