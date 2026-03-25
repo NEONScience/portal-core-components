539a88ec5f4d7c132199217066f6ccb98f8f6a9d
jsampson <jsampson@battelleecology.org>
2026-03-25 17:03:31 -0600

    use babel for lib builds only

--- server.mjs
+++ server.mjs
@@ -5,8 +5,8 @@ const app = express();
 const serveApp = express.static(path.join(import.meta.dirname, 'build'));
 
 app.use('/core-components-next', serveApp);
-app.use('/core-components-next/*', serveApp);
-app.get('/*', (req, res) => {
+app.use('/core-components-next/{*splat}', serveApp);
+app.get('/{*splat}', (req, res) => {
   res.status(404).send();
 });
 app.listen(3000);
